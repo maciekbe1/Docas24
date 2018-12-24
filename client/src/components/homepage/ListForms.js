f6fd349900cc48c5eae40a356a2d4d28009ffa38
Piotr Frukacz <pfrukacz@gmail.com>
2018-12-24 13:22:32 +0100

    Add styles newest forms

--- client/src/components/homepage/ListForms.js
+++ client/src/components/homepage/ListForms.js
@@ -17,15 +17,16 @@ class ListForm extends Component {
             <section className='form-filter'>
                     {forms.slice(0, this.state.count).map(form => {
                      return (
-                        <div className={form.class} key={form.id}>
+                        <div className={form.class + " col-xl-4 col-lg-6"} key={form.id}>
                             <div className="square">
-                                <ul className="nav">
-                                    <li className="nav-item">Zapisz</li>
-                                    <li className="nav-item">Oceń</li>
-                                    <li className="nav-item">Wypełnij</li>
+                                <span class="badge badge-primary">Nowość</span>
+                                <ul className="nav square-btns">
+                                    <li className="btn-link">Zapisz</li>
+                                    <li className="btn-link">Oceń</li>
+                                    <li className="btn-primary">Wypełnij</li>
                                 </ul>
                             </div>
-                            <h3>{form.name}</h3>
+                            <h3 className="font-weight-bold">{form.name}</h3>
                         </div>
                      )
                     })}
