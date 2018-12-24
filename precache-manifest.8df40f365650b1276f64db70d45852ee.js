278980b1265e1f7f1ca86ad4e503f43d4bc0bb3a
Maciej Bednarczyk <m.bednarczyk.1991@gmail.com>
2018-12-24 13:41:35 +0100

    Updates

---
+++ precache-manifest.8df40f365650b1276f64db70d45852ee.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "3786422f6d3fbfb8608f",
+    "url": "/Docas24/static/css/main.976e1764.chunk.css"
+  },
+  {
+    "revision": "3786422f6d3fbfb8608f",
+    "url": "/Docas24/static/js/main.3786422f.chunk.js"
+  },
+  {
+    "revision": "548b7f2c272c9189ba72",
+    "url": "/Docas24/static/js/1.548b7f2c.chunk.js"
+  },
+  {
+    "revision": "4498b6a38f709f2ec673",
+    "url": "/Docas24/static/js/runtime~main.4498b6a3.js"
+  },
+  {
+    "revision": "4fe82ef62aca5c07f89061dd2733317c",
+    "url": "/Docas24/static/media/docas_logo.4fe82ef6.png"
+  },
+  {
+    "revision": "5e1ba8892c1dbeb569a796788c2057b9",
+    "url": "/Docas24/static/media/01_Home_bg.5e1ba889.png"
+  },
+  {
+    "revision": "0ff41a4066806d2e607e55479428749b",
+    "url": "/Docas24/static/media/01_Home_computer.0ff41a40.png"
+  },
+  {
+    "revision": "da1d2e2f266b111a14f28e0e324e65d6",
+    "url": "/Docas24/static/media/Niramit-Regular.da1d2e2f.ttf"
+  },
+  {
+    "revision": "3858981791d4337c178b970d9859eef4",
+    "url": "/Docas24/index.html"
+  }
+];
