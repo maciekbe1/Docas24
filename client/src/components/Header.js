328dd89b6f64b74809a5a74a0ae05410a490c602
Piotr Frukacz <pfrukacz@gmail.com>
2018-12-27 13:53:54 +0100

    Add styles, rebuild navbar, add hamburger in mobile, rebuild statistics

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -7,31 +7,36 @@ const Header = (props) => {
     return (
         <header className="header container-fluid">
 
-            <nav className="nav navbar-expand-lg">
 
-                <Link className="navbar-brand col-xl-2 offset-xl-1" to="/">
+            
+            <nav className="navbar navbar navbar-light bg-light navbar-expand-lg">
+                <Link className="navbar-brand offset-xl-1" to="/">
                     <img alt="logo" className="logo" src={logo} />
                 </Link>
-
-                <ul className="navbar-nav col-xl-5 offset-xl-1">
-                    <li className="nav-item">
-                        <NavLink activeClassName="selected" to="/pages/partners">Partnerzy</NavLink>
-                    </li>
-                    <li className="nav-item">
-                        <a href="http://localhost/index.php/site/register">Rejestracja</a>
-                    </li>
-                    <li className="nav-item">
-                        <NavLink activeClassName="selected" to="/pages/help">Pomoc</NavLink>
-                    </li>
-                    <li className="nav-item">
-                        <NavLink activeClassName="selected" to="/pages/demo">Demo</NavLink>
-                    </li>
-                    <li className="nav-item">
-                        <NavLink activeClassName="selected" to="/pages/contact">Kontakt</NavLink>
-                    </li>
-                </ul>
-                <div className="nav-log-in col-lg-2">
-                    <a className="btn btn-outline-primary log-in" href="https://b2ng.bpower2.com/index.php/site/login">Zaloguj się</a>
+                <button className="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarCollapse">
+                    <ul className="navbar-nav offset-xl-2">
+                        <li className="nav-item">
+                            <NavLink activeClassName="selected" to="/pages/partners">Partnerzy</NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <a href="http://localhost/index.php/site/register">Rejestracja</a>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink activeClassName="selected" to="/pages/help">Pomoc</NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink activeClassName="selected" to="/pages/demo">Demo</NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink activeClassName="selected" to="/pages/contact">Kontakt</NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <a className="btn btn-outline-primary log-in" href="https://b2ng.bpower2.com/index.php/site/login">Zaloguj się</a>
+                        </li>
+                    </ul>
                 </div>
             </nav>
         </header>
--- client/src/components/homepage/FindForm.js
+++ client/src/components/homepage/FindForm.js
@@ -11,22 +11,20 @@ class FindForm extends React.Component {
     }
     render(){
         return (
-            <div className="container">
+            <div className="container-fluid find-form">
                 <div className="row">
-                    <div className="col-lg-6 block-find-form">
-                        <h1>Znajdź formularz:</h1>
-                        <div className="input-group mb-3">
-                          <input value={this.state.search} onChange={this.onChange} type="text" className="form-control docas-input" placeholder="Wpisz jakiego formularza szukasz" aria-label="Recipient's username" aria-describedby="basic-addon2" />
-                          <div className="input-group-append">
-                            <span className="input-group-text" id="basic-addon2">
-                            <i className="fas fa-search fa-2x"></i>
-                            </span>
-                          </div>
+                    <img alt="home" className="find-form-image" src={home_bg} />
+                </div>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-lg-6 block-find-form">
+                            <h1>Znajdź formularz:</h1>
+                            <div className="input-group mb-3">
+                              <input value={this.state.search} onChange={this.onChange} type="text" className="form-control docas-input" placeholder="Wpisz jakiego formularza szukasz" aria-label="Recipient's username" aria-describedby="basic-addon2" />
+                              <i className="fas fa-search fa-2x"></i>
+                            </div>
+                            <Link to={{ pathname: '/pages/forms', state: { foo: this.state.search} }} className="btn btn-primary docas-find-form-button">SZUKAJ</Link>
                         </div>
-                        <Link to={{ pathname: '/pages/forms', state: { foo: this.state.search} }} className="btn btn-primary docas-find-form-button">SZUKAJ</Link>
-                    </div>
-                    <div className="col-lg-6">
-                        <img alt="home" className="find-form-image" src={home_bg} />
                     </div>
                 </div>
             </div>
