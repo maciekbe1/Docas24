4ffad78aa40d775217e6cf1740033c23df20939a
Piotr Frukacz <pfrukacz@gmail.com>
2019-01-16 12:09:54 +0100

    Change styles filters, add function to dynamic add filter

--- client/src/components/forms/FormsFilters.js
+++ client/src/components/forms/FormsFilters.js
@@ -9,34 +9,40 @@ class Formsfilters extends Component {
         return (
             <div className="filter-options">
                 <div className="filter-option">
+                    <div className={this.props.departmentOptions.length > 1 ? "filter" : "d-none"}>
+                        <label>Urząd</label>
+                        <select id="Department" onChange={this.changeOption.bind(this, 'department')}>
+                            {this.props.departmentOptions.map( (department, index) => {
+                                return <option key={index}>{department}</option>
+                            })}
+                        </select>
+                    </div>
+                    <div className={this.props.typeOptions.length > 1 ? "filter" : "d-none"}>
+                        <label>Rodzaj dokumentu</label>
+                        <select id="Type" onChange={this.changeOption.bind(this, 'type')}>
+                            {this.props.typeOptions.map( (type, index) => {
+                                return <option key={index}>{type}</option>
+                            })}
+                        </select>
+                    </div>
 
-                    <label>Urząd</label>
-                    <select id="Department" onChange={this.changeOption.bind(this, 'department')}>
-                        {this.props.departmentOptions.map( (department, index) => {
-                            return <option key={index}>{department}</option>
-                        })}
-                    </select>
+                    <div className={this.props.categoryOptions.length > 1 ? "filter" : "d-none"}>
+                        <label>Kategoria sprawy</label>
+                        <select id="Category" onChange={this.changeOption.bind(this, 'category')}>
+                            {this.props.categoryOptions.map( (category, index) => {
+                            return <option key={index}>{category}</option>
+                            })}
+                        </select>
+                    </div>
 
-                    <label>Rodzaj dokumentu</label>
-                    <select id="Type" onChange={this.changeOption.bind(this, 'type')}>
-                        {this.props.typeOptions.map( (type, index) => {
-                            return <option key={index}>{type}</option>
-                        })}
-                    </select>
-
-                    <label>Kategoria sprawy</label>
-                    <select id="Category" onChange={this.changeOption.bind(this, 'category')}>
-                        {this.props.categoryOptions.map( (category, index) => {
-                           return <option key={index}>{category}</option>
-                        })}
-                    </select>
-
-                    <label>Rok</label>
-                    <select id="Year" onChange={this.changeOption.bind(this, 'year')}>
-                        {this.props.yearOptions.map( (year, index) => {
-                            return <option key={index}>{year}</option>
-                        })}
-                    </select>
+                    <div className={this.props.yearOptions.length > 1 ? "filter" : "d-none"}>
+                        <label>Rok</label>
+                        <select id="Year" onChange={this.changeOption.bind(this, 'year')}>
+                            {this.props.yearOptions.map( (year, index) => {
+                                return <option key={index}>{year}</option>
+                            })}
+                        </select>
+                    </div>
 
                 </div>
             </div>
