02bf1677f30ae0fa05b80061682f1d8a0e7774b4
Piotr Frukacz <pfrukacz@gmail.com>
2019-01-17 12:54:05 +0100

    Add success send message component, configure contact form  and add validation form

--- client/src/components/contact/Form.js
+++ client/src/components/contact/Form.js
@@ -5,7 +5,11 @@ import RadioButton from './RadioButton';
 class Form extends Component {
     state = {
         rules: false,
-        copy: false
+        copy: false,
+        name: "",
+        email: "",
+        phone: "",
+        text: ""
     }
     changeForm = (e) => {
         const inputState = {};
@@ -24,21 +28,29 @@ class Form extends Component {
     }
 
     sendForm = () => {
-
+        var rules = document.querySelector('.form-checkbox-btn p');
+        if (!this.state.rules) {
+            rules.classList.add("text-danger")
+        } else {
+            rules.classList.remove("text-danger")
+        }
     }
 
     render() {
         return (
-            <form>
-                <input onChange={this.changeForm} className="col-12" type="text" name="name"placeholder="Imię i nazwisko" />
-                <input onChange={this.changeForm} className="col-6" type="text" name="email" placeholder="Adres e-mail" />
-                <input onChange={this.changeForm} className="col-6" type="text" name="phone" placeholder="Numer telefonu" />
-                <textarea onChange={this.changeForm} className="col-12" name="text" placeholder="Treść wiadomości"></textarea>
+            <form action="https://formspree.io/&#112;&#105;&#111;&#116;&#114;&#46;&#102;&#114;&#117;&#107;&#97;&#99;&#122;&#46;&#115;&#103;&#64;&#103;&#109;&#97;&#105;&#108;&#46;&#99;&#111;&#109;" method="POST">
+                <input onChange={this.changeForm} className="col-12 contact-field" type="text" name="name"placeholder="Imię i nazwisko" required />
+                <input onChange={this.changeForm} className="col-6 contact-field" type="email" name="email" placeholder="Adres e-mail" required />
+                <input onChange={this.changeForm} className="col-6 contact-field" type="text" pattern="\d+" name="phone" placeholder="Numer telefonu" />
+                <textarea onChange={this.changeForm} className="col-12 contact-field" name="text" placeholder="Treść wiadomości" required></textarea>
                 <div className="form-checkbox-btns">
                     <RadioButton name="rules" action={this.rulesChange} text={"Zapoznałem się z Regulaminem i Polityką Prywatności"} />
                     <RadioButton name="copy" action={this.copyChange} text={"Wyślij kopie do mnie"} />
+                    <input type="hidden" name="_cc" value={this.state.copy ? this.state.email : ""} />
+                    <input type="hidden" name="_language" value="pl" />
+                    <input type="hidden" name="_next" value="http://docas24.com/#/success"/>
                 </div>
-                <button onClick={this.sendForm} className="btn docas-btn-primary float-right">Wyślij</button>
+                <input onClick={this.sendForm} type={this.state.rules ? "submit" : "button"} value="Wyślij" className="btn docas-btn-primary float-right" />
             </form>
         );
     }
---
+++ client/src/components/pages/ThankYou.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const ThankYou = () => {
+    return (
+        <div className="constainer send-message-success">
+            <div className="row">
+                <h2>Twoja wiadomość została wysłana.</h2>
+                <h3>Dziękujemy za zainteresowanie</h3>
+                <h3>w najbliższym czasie skontaktujemy się z Państwem.</h3>
+            </div>
+        </div>
+    );
+}
+
+export default ThankYou;
